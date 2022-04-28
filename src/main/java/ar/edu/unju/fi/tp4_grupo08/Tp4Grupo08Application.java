241c91202f02316f50715e291cee7b1337c290f0
Rocio <roaguerrero17@gmail.com>
2022-04-28 11:31:50 -0300

    Initial commit

---
+++ src/main/java/ar/edu/unju/fi/tp4_grupo08/Tp4Grupo08Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp4_grupo08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo08Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp4Grupo08Application.class, args);
+    }
+
+}
---
+++ src/test/java/ar/edu/unju/fi/tp4_grupo08/Tp4Grupo08ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp4_grupo08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4Grupo08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
